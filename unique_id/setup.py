ced353cd702e72123a1ce1b0965038d2d90a753f
Jack O'Quin <jack.oquin@gmail.com>
2013-03-25 11:54:58 -0500

    fix unique_id Python package install problem (#4)

--- unique_id/setup.py
+++ unique_id/setup.py
@@ -6,7 +6,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 d = generate_distutils_setup(
     packages=['unique_id'],
     package_dir={'': 'src'},
-    install_requires=['genpy', 'uuid'],
+    install_requires=['rospy', 'uuid',  'uuid_msgs'],
     )
 
 setup(**d)
